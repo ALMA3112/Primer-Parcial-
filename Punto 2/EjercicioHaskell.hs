298729ef7b4e77723b5d5884140d17d296d8081c
ALMA3112 <carlispulido@gmail.com>
2024-09-06 17:30:58 -0500

    Haskell

--- Punto 2/EjercicioHaskell.hs
+++ Punto 2/EjercicioHaskell.hs
@@ -1,9 +1,11 @@
 import Data.List (sortBy)
-import Data.Ord (comparing)
+      import Data.Ord (comparing)
 
-ordenarEstudiantes :: [(String, Int)] -> [(String, Int)]
-ordenarEstudiantes = sortBy (\(name1, grade1) (name2, grade2) ->
-    compare grade2 grade1 <> compare name1 name2)
+      ordenarEstudiantes :: [(String, Int)] -> [(String, Int)]
+      ordenarEstudiantes = sortBy (\(name1, grade1) (name2, grade2) ->
+      compare grade2 grade1 <> compare name1 name2)
 
-students = [("Ana", 85), ("Luis", 90), ("Carlos", 85), ("Beto", 90), ("David", 95)]
-main = print (ordenarEstudiantes students)
+      students :: [(String, Int)]
+      students = [("Ana", 85), ("Luis", 90), ("Carlos", 85), ("Beto", 90), ("David", 95)]
+      main :: IO ()
+      main = print (ordenarEstudiantes students)
